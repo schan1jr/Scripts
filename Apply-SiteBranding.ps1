3212b8e97473cfaa011729b7c4a39afafb2efcce
schan1jr <74208028+schan1jr@users.noreply.github.com>
2023-05-24 08:13:13 -0500

    Add files via upload

---
+++ Apply-SiteBranding.ps1
@@ -0,0 +1,41 @@
+﻿############################################################
+#
+#   Description: Site Provisioning Script
+#
+#   Author: Jeffrey Schantz, Concurrency Inc.
+#
+#   Date: 8/8/2019
+#
+############################################################
+
+$teamSiteUrl = "<SiteURL>"
+
+$credentials = get-credential "<AccountName>" 
+
+#Connect to SPO site
+Connect-PnPOnline $teamSiteUrl -Credentials $credentials
+
+# Now we have access on the SharePoint site for any operations
+$context = Get-PnPContext
+$web = Get-PnPWeb
+$context.Load($web, $web.WebTemplate)
+Invoke-PnPQuery
+
+
+#Connect Site to a New Office Group
+Connect-SPOService -Url "<SP Admin Center URL>" -Credential $credentials 
+$site = Get-SPOSite -Identity $teamSiteUrl
+Set-SPOSiteOffice365Group -Site $site -DisplayName $web.Title -Alias $web.Title -IsPublic $false
+Disconnect-SPOService
+
+#Apply Provisioning Template
+write-host "Applying Provisioning Template" -ForegroundColor Green
+Apply-PnPProvisioningTemplate -Path "Filename.xml" -Web $web #Team Site
+
+
+#Set Theme
+write-host "Setting Theme" -ForegroundColor Green
+$design = Get-PnPSiteDesign -Identity "<Theme Name>"
+Invoke-PnPSiteDesign -Identity $design -WebUrl $web.Url
+
+Disconnect-PnPOnline
---
+++ get-LargeLists.ps1
@@ -0,0 +1,28 @@
+﻿ $siteUrl = "https://<Tenant>-admin.sharepoint.com/"
+ $creds = Get-Credential
+
+ Connect-SPOService -url $siteUrl -Credential $creds
+
+ $sites= Get-SPOSite -Limit ALL
+
+$FoundLists = @{}
+
+foreach($site in $sites)
+{
+
+    Connect-PnPOnline -Url $site.Url -UseWebLogin #-Credentials $creds
+    Write-Host $site.Url
+    $pnpsite = Get-PnpSite -ErrorAction Ignore
+    $lists = Get-PnPList
+    foreach($list in $lists)
+    {
+        if($list.ItemCount -gt 5000)
+        {
+            Write-Host "Large List"
+            $FoundLists.Add($list.Path)
+        }
+    }
+    Disconnect-PnPOnline
+}
+
+$FoundLists | Export-Csv C:\Temp\LargeLists.csv
